f7627fafb4bc6e070982ce8d1cf45c3d995b37f5
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2025-11-24 10:26:59 -0500

    Add PokemonPicture component and update PokemonGame layout
    - Created a new PokemonPicture.vue component with a basic template structure.
    - Updated PokemonGame.vue to include a new section for displaying the Pokémon question and placeholders for the Pokémon picture and options.

--- 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
+++ 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
@@ -1,7 +1,12 @@
 <template>
-  <section class="flex flex-col items-center justify-center h-screen">
+  <section v-if="false" class="flex flex-col items-center justify-center h-screen w-screen">
     <h1 class="text-3xl">Espere por favor...</h1>
     <h3 class="animate-pulse">Cargando pokemons...</h3>
+  </section>
+  <section class="flex flex-col items-center justify-center h-screen w-screen">
+    <h1 class="text-3xl">Quién es ese Pokémon?</h1>
+    <!-- Pokemin Picture -->
+    <!-- Pokemon Options -->
   </section>
 </template>
 
