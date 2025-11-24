7a86a0858d78c6e66c66c81d0ca9f4090fdb2bd4
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2025-11-24 10:24:05 -0500

    Integrate Tailwind CSS into the project
    - Updated .prettierrc.json to enforce semicolons and added tabWidth and trailingComma settings.
    - Added Tailwind CSS and related dependencies to package.json and package-lock.json.
    - Created tailwind.config.js for Tailwind CSS configuration.
    - Updated vite.config.ts to include Tailwind CSS as a plugin.
    - Refactored App.vue to use the new PokemonGame component.
    - Added styles.css to import Tailwind CSS styles.
    - Created PokemonGame.vue component for loading state display.

---
+++ 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-3xl">Espere por favor...</h1>
+    <h3 class="animate-pulse">Cargando pokemons...</h3>
+  </section>
+</template>
+
+<script setup lang="ts">
+</script>
