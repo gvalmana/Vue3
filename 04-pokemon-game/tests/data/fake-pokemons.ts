2ffe547470ed8ea059bd3581153058b8202ef684
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2026-01-19 15:29:08 -0500

    Add axios-mock-adapter for testing and create fake Pokémon data
    - Introduced axios-mock-adapter to facilitate mocking API requests in tests.
    - Added a new fake-pokemons.ts file containing a list of mock Pokémon data for testing purposes.
    - Updated usePokemonGame.test.ts to include additional tests for game logic and state management, ensuring comprehensive coverage of the composable's functionality.

---
+++ 04-pokemon-game/tests/data/fake-pokemons.ts
@@ -0,0 +1,14 @@
+const pokemonListFake = [
+    { name: 'pokemon1', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+    { name: 'pokemon2', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+    { name: 'pokemon3', url: 'https://pokeapi.co/api/v2/pokemon/3/' },
+    { name: 'pokemon4', url: 'https://pokeapi.co/api/v2/pokemon/4/' },
+    { name: 'pokemon5', url: 'https://pokeapi.co/api/v2/pokemon/5/' },
+    { name: 'pokemon6', url: 'https://pokeapi.co/api/v2/pokemon/6/' },
+    { name: 'pokemon7', url: 'https://pokeapi.co/api/v2/pokemon/7/' },
+    { name: 'pokemon8', url: 'https://pokeapi.co/api/v2/pokemon/8/' },
+    { name: 'pokemon9', url: 'https://pokeapi.co/api/v2/pokemon/9/' },
+    { name: 'pokemon10', url: 'https://pokeapi.co/api/v2/pokemon/10/' },
+];
+
+export { pokemonListFake };
--- 04-pokemon-game/tests/modules/pokemon/composables/usePokemonGame.test.ts
+++ 04-pokemon-game/tests/modules/pokemon/composables/usePokemonGame.test.ts
@@ -1,24 +1,90 @@
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { usePokemonGame } from '@/modules/pokemon/composables/usePokemonGame';
 import { withSetup } from "../utils/with-setup";
 import { GameStatus } from "@/modules/pokemon/interfaces";
 import { flushPromises } from "@vue/test-utils";
+import { pokemonApi } from "@/modules/pokemon/api/pokemonApi";
+import MockAdapter from "axios-mock-adapter";
+import { pokemonListFake } from "../../../data/fake-pokemons";
+import confetti from 'canvas-confetti';
+
+const mockPokemonsApi = new MockAdapter(pokemonApi);
+
+mockPokemonsApi.onGet('/?limit=151').reply(200,{results: pokemonListFake});
+
+vi.mock('canvas-confetti', () => ({
+    default: {
+        confetti: vi.fn(),
+    },
+}));
 
 describe('usePokemonGame', () => {
+    beforeEach(() => {
+        vi.restoreAllMocks();
+    });
+
     test('should initialize with the correct default values', async () => {
-        const [result, app ]= await withSetup<ReturnType<typeof usePokemonGame>>(usePokemonGame);
+        const [result, app ]= await withSetup(usePokemonGame);
         expect(result.gameStatus.value).toBe(GameStatus.Playing);
         expect(result.isLoading.value).toBeTruthy();
         expect(result.pokemonOptions.value).toEqual([]);
         expect(result.randomPokemon.value).toBeUndefined();
-        
-        await new Promise(r=> setTimeout(r, 1000));
+
         await flushPromises();
+
         expect(result.isLoading.value).toBeFalsy();
         expect(result.pokemonOptions.value.length).toBe(4);
         expect(result.randomPokemon.value).toEqual({
             id: expect.any(Number),
             name: expect.any(String),
         });
+    });
+
+    test('should correctly handle getNextRound and return different pokemon', async () => {
+        const [ result ]= await withSetup(usePokemonGame);
+        await flushPromises();
+
+        result.gameStatus.value = GameStatus.Won;
+
+        // Estímulo
+        result.getNextRound(5);
+        expect(result.gameStatus.value).toBe(GameStatus.Playing);
+        expect(result.pokemonOptions.value).toHaveLength(5);
+
+    });
+
+    test('should correctly handle getNextRound', async () => {
+        const [ result ]= await withSetup(usePokemonGame);
+        await flushPromises();
+        const firstOptions = result.pokemonOptions.value.map(p => p.name);
+        result.getNextRound();
+        const secondOptions = result.pokemonOptions.value.map(p => p.name);
+        secondOptions.forEach(pokemon => {
+            expect(firstOptions).not.toContain(pokemon);
+        })
+        expect(secondOptions).not.toEqual(firstOptions);
+    });
+
+    test('should correctly handle an incorrect answer checkAnswer', async () => {
+        const [ result ]= await withSetup(usePokemonGame);
+        await flushPromises();
+        const { checkAnswer, gameStatus } = result;
+        expect(gameStatus.value).toBe(GameStatus.Playing);
+        checkAnswer(10000000); // Pokemon that does not exist
+        expect(gameStatus.value).toBe(GameStatus.Lost);
+    });
+
+    test('should correctly handle a correct answer checkAnswer', async () => {
+        const [ result ]= await withSetup(usePokemonGame);
+        await flushPromises();
+        const { checkAnswer, gameStatus, randomPokemon } = result;
+        expect(gameStatus.value).toBe(GameStatus.Playing);
+        checkAnswer(randomPokemon.value?.id ?? 0);
+        expect(confetti).toHaveBeenCalledWith(expect.objectContaining({
+            particleCount: 300,
+            spread: 150,
+            origin: { y: 0.6 },
+        }));
+        expect(gameStatus.value).toBe(GameStatus.Won);
     });
 });
