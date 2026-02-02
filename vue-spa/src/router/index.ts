3443c6b869af6cdca5f7149f3f89cbbca2fbf42d
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2026-02-02 17:34:41 -0500

    Refactor App.vue layout and add LoginPage and LandingLayout components
    - Simplified App.vue by removing unnecessary layout elements and retaining only the RouterView.
    - Introduced LoginPage.vue for user authentication with a responsive design and form elements.
    - Added LandingLayout.vue to serve as a new layout for the landing page, incorporating a header, footer, and main content area.
    - Updated router configuration to utilize LandingLayout for the home route and added a route for the login page.

--- vue-spa/src/router/index.ts
+++ vue-spa/src/router/index.ts
@@ -2,22 +2,32 @@ import HomePage from "@/modules/landing/pages/HomePage.vue";
 import PricingPage from "@/modules/landing/pages/PricingPage.vue";
 import ContactPage from "@/modules/landing/pages/ContactPage.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import LandingLayout from "@/modules/landing/layouts/LandingLayout.vue";
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/', component: HomePage, name: 'home',
+            path: '/', component: LandingLayout, name: 'landing',
+            children: [
+                {
+                    path: '/', component: HomePage, name: 'home',
+                },
+                {
+                    path: '/features', component: () => import('@/modules/landing/pages/FeaturesPage.vue'), name: 'features',
+                },
+                {
+                    path: '/pricing', component: () => import('@/modules/landing/pages/PricingPage.vue'), name: 'pricing',
+                },
+                {
+                    path: '/contact', component: () => import('@/modules/landing/pages/ContactPage.vue'), name: 'contact',
+                },
+            ]
         },
+        // Auth
         {
-            path: '/features', component: () => import('@/modules/landing/pages/FeaturesPage.vue'), name: 'features',
-        },
-        {
-            path: '/pricing', component: () => import('@/modules/landing/pages/PricingPage.vue'), name: 'pricing',
-        },
-        {
-            path: '/contact', component: () => import('@/modules/landing/pages/ContactPage.vue'), name: 'contact',
-        },
+            path: '/auth', component: () => import('@/modules/auth/pages/LoginPage.vue'), name: 'login',
+        }
     ],
 });
 
