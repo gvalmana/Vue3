eab3cc5471f7d79b1d5010caa7391ea4348eb93a
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2026-07-24 11:08:45 -0500

    tercer commit

---
+++ 06-projects-app/src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col">
+    <top-menu />
+    <main>
+      <side-menu />
+      <route-view />
+    </main>
+  </div>
+</template>
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '@/modules/projects/components/SideMenu.vue';
+</script>
