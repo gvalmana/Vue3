f939ba5e7bc3de3839a917109940a0a165bfedb3
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2026-07-26 19:33:00 -0500

    otro commit

--- 06-projects-app/src/modules/projects/layouts/ProjectsLayout.vue
+++ 06-projects-app/src/modules/projects/layouts/ProjectsLayout.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="flex flex-col">
     <top-menu />
-    <main>
+    <main class="flex flex-row">
       <side-menu />
-      <route-view />
+      <router-view />
     </main>
   </div>
 </template>
 <script setup lang="ts">
 import TopMenu from '@/modules/common/components/TopMenu.vue';
 import SideMenu from '@/modules/projects/components/SideMenu.vue';
+import { RouterView } from 'vue-router';
 </script>
---
+++ 06-projects-app/src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="overflow-x-auto w-full">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avances</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 2 -->
+        <tr class="hover:bg-base-300">
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <fab-button> <add-circle /> </fab-button>
+</template>
+<script setup lang="ts">
+import FabButton from '@/modules/common/components/FabButton.vue';
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+</script>
