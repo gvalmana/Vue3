6a9cc217f741e9e1587e9d3895ccbc6e3b928b1d
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2026-09-17 09:31:44 -0500

    feat: add TopMenu and ShopLayout components with routing setup
    - Created TopMenu.vue for the navigation bar with login and register buttons.
    - Implemented ShopLayout.vue to display product listings and a footer.
    - Updated router to include a route for the shop layout as the main entry point.

--- 07-admin-shop/src/App.vue
+++ 07-admin-shop/src/App.vue
@@ -2,9 +2,6 @@ import { RouterView } from 'vue-router';
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
   <RouterView />
 </template>
 
--- 07-admin-shop/src/router/index.ts
+++ 07-admin-shop/src/router/index.ts
@@ -1,8 +1,15 @@
+import ShopLayout from '../modules/shop/layouts/ShopLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'shop',
+      component: ShopLayout
+    }
+  ],
 })
 
 export default router
