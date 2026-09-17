49cdbc514c3154420f284c22fc537d5f6f80da11
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2026-09-17 09:09:43 -0500

    feat: update App.vue to display Spanish greeting and add main.css file

--- 07-admin-shop/src/App.vue
+++ 07-admin-shop/src/App.vue
@@ -1,11 +1,9 @@
+import { RouterView } from 'vue-router';
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Hola mundo</h1>
+  <RouterView />
 </template>
 
 <style scoped></style>
